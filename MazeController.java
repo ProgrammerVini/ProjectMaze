5739b4d3ba90b4b98650d0fab21a1219ea81fe29
ProgrammerVini <122817619+ProgrammerVini@users.noreply.github.com>
2023-01-21 16:58:39 +0100

    Add files via upload

--- MazeController.java
+++ MazeController.java
@@ -8,67 +8,9 @@ import java.awt.event.KeyListener;
 public class MazeController extends JFrame {
     private static final int WINDOW_WIDTH = 500;
     private static final int WINDOW_HEIGHT = 500;
-    private static final int CELL_WIDTH = WINDOW_WIDTH / MazeModel.getWidth();
-    private static final int CELL_HEIGHT = WINDOW_HEIGHT / MazeModel.getHeight();
     private MazeModel mazeModel;
     private MazeView mazeView;
 
-    public MazeController() {
-        mazeModel = new MazeModel(20, 20);
-        int playerX = (int)(Math.random() * mazeModel.getWidth());
-        int playerY = (int)(Math.random() * mazeModel.getHeight());
-        mazeView = new MazeView(mazeModel, playerX, playerY);
-
-        setTitle("Labyrinthe");
-        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(false);
-
-        add(mazeView);
-        addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyPressed(KeyEvent e) {
-                //handle arrow key input
-		if (e.getKeyCode() == KeyEvent.VK_UP) {
-		    if (!cells[playerX][playerY].north) {
-			playerY--;
-		    }
-		} else if (e.getKeyCode() == KeyEvent.VK_DOWN) {
-		    if (!cells[playerX][playerY].south) {
-			playerY++;
-		    }
-		} else if (e.getKeyCode() == KeyEvent.VK_LEFT) {
-		    if (!cells[playerX][playerY].west) {
-			playerX--;
-		    }
-		} else if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
-		    if (!cells[playerX][playerY].east) {
-			playerX++;
-		    }
-		}
-            }
-        });
-
-        setVisible(true);
-    }
-
-    public static void main(String[] args) {
-        new MazeController();
-    }
-}
-
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
-
-
-public class MazeController extends JFrame {
-    private static final int WINDOW_WIDTH = 500;
-    private static final int WINDOW_HEIGHT = 500;
-    private MazeModel mazeModel;
-    private MazeView mazeView;
     public MazeController() {
     mazeModel = new MazeModel(20, 20);
     int playerX = (int)(Math.random() * mazeModel.getWidth());
